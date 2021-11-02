c446ad3ffb78f272290cd3bd78f082766808fc50
Alexey Yunoshev <ayunoshev@upteam.com>
2021-11-02 09:19:50 +0300

    rm nat

--- main.tf
+++ main.tf
@@ -174,8 +174,8 @@ module "subnets" {
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
   cidr_block           = module.vpc.vpc_cidr_block
-  # nat_gateway_enabled  = var.nat_gateway_enabled
-  # nat_instance_enabled = ! var.nat_gateway_enabled
+  nat_gateway_enabled  = false
+  nat_instance_enabled = false
   tags                 = module.base_label.tags
 }
 
